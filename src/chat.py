f704e1f26e35017406157090a49e78f28d988fd3
Derpman <91960533+DerpmanDev@users.noreply.github.com>
2024-09-15 16:11:33 -0400

    pai-001 > gpt-3.5-turbo

--- src/chat.py
+++ src/chat.py
@@ -1,10 +1,13 @@
 # chat.py
 import openai
+from .functions import load_ai_name
+
+user = load_ai_name()
 
 openai.api_key = ""
 openai.api_base = "https://api.pawan.krd/cosmosrp/v1"
 
-AI_MODEL = "pai-001"
+AI_MODEL = "gpt-3.5-turbo"
 
 
 def chat_with_gpt(prompt):
