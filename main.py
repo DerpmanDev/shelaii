a2f9fe98872b4d9321ed2637be78806fbf02465f
GitHub Staff <91960533+DerpmanDev@users.noreply.github.com>
2024-09-15 22:22:33 -0400

    chore: set event loop policy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src import app
+import asyncio
 
+if hasattr(asyncio, 'WindowsSelectorEventLoopPolicy'):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 def main():
     app.main()
